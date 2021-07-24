6387b43a8b41670c4f0faa12499bfc72d4b2e66f
[Andrew Core] <[adcore89@gmail.com]>
2021-07-24 16:26:25 -0400

    carousel works

---
+++ src/components/SliderData.js
@@ -0,0 +1,14 @@
+export const SliderData = [
+  {
+    image:
+      "https://images.unsplash.com/photo-1627135024241-e4b70d9196b0?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHwyfHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=400&q=60",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1627001158145-d9d7cb999d0d?ixid=MnwxMjA3fDB8MHxlZGl0b3JpYWwtZmVlZHw0fHx8ZW58MHx8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=500&q=60",
+  },
+  {
+    image:
+      "https://images.unsplash.com/photo-1626881255770-2397375aad8d?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=632&q=80",
+  },
+];
